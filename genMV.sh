5c1e90a6d9c1e7814f750088d39286a2cc055b8c
nethan789 <nethanprofree@gmail.com>
2025-09-11 08:46:26 -0500

    Update genMV.sh

--- genMV.sh
+++ genMV.sh
@@ -6,27 +6,32 @@ case "$1" in
 
         VBoxManage list vms
         ;;
+
     "N")
         if [ -z "$2" ]; then
             echo "Erreur : Nom de la machine manquant pour N"
             exit 1
         fi
 
-VBoxManage createvm --name "Debian1" --ostype "$2" --register
-VBoxManage modifyvm "Debian1" --memory 4096 --vram 16 --nic1 nat --boot1 net
-VBoxManage createmedium disk --filename "Debian1.vdi" --size 64
-VBoxManage storagectl "Debian1" --name "SATA" --add sata
-VBoxManage storageattach "Debian1" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "Debian1.vdi"
+        VBoxManage createvm --name "Debian1" --ostype "$2" --register
+        VBoxManage modifyvm "Debian1" --memory $RAM --vram 16 --nic1 nat --boot1 net
+        VBoxManage createmedium disk --filename "Debian1.vdi" --size $DD_SIZE
+        VBoxManage storagectl "Debian1" --name "SATA" --add sata
+        VBoxManage storageattach "Debian1" --storagectl "SATA" --port 0 --device 0 --type hdd --medium "Debian1.vdi"
+        echo "VM Debian1 créée..."
+        ;;
 
-  
-   "S")
+    "S")
         if VBoxManage list vms | grep -q '"Debian1"'; then
              echo "Une MV Debian1 existe déjà. Suppression en cours..."
              VBoxManage unregistervm "Debian1" --delete
+             echo "VM supprimée."
+        else
+             echo "Aucune VM Debian1 trouvée."
         fi
+        ;;
 
-echo "VM Debian1 créée..."
-read
-
-VBoxManage unregistervm "Debian1" --delete
-echo "VM supprimée."
+    *)
+        echo "Usage: $0 {L|N <os_type>|S}"
+        ;;
+esac
